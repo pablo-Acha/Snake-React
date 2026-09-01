8631dff1d296dc87b52db2dfab63989f75e98aed
pablo-Acha <pabloate901@gmail.com>
2026-09-01 17:54:31 -0400

    tablero con map

--- src/Tablero.tsx
+++ src/Tablero.tsx
@@ -13,34 +13,33 @@ interface TableroProps {
 const TAMANO_TABLERO = 8;
 
 export default function Tablero({ serpiente, comida }: TableroProps) {
-  const filas = [];
-
-  for (let fila = 0; fila < TAMANO_TABLERO; fila++) {
-    const celdasFila = [];
-
-    for (let columna = 0; columna < TAMANO_TABLERO; columna++) {
-      const esCabeza = serpiente[0].x === columna && serpiente[0].y === fila;
-      const esCuerpo = serpiente.slice(1).some(
-        (segmento) => segmento.x === columna && segmento.y === fila
-      );
-      const esComida = comida.x === columna && comida.y === fila;
-
-      let claseCelda = 'celda';
-      if (esCabeza) claseCelda += ' cabeza';
-      else if (esCuerpo) claseCelda += ' cuerpo';
-      else if (esComida) claseCelda += ' comida';
-
-      celdasFila.push(
-        <td key={`${fila}-${columna}`} className={claseCelda} />
-      );
-    }
-
-    filas.push(<tr key={`fila-${fila}`}>{celdasFila}</tr>);
-  }
+  const filas = Array.from({ length: TAMANO_TABLERO }, (_, i) => i);
+  const columnas = Array.from({ length: TAMANO_TABLERO }, (_, i) => i);
 
   return (
     <table className="tablero">
-      <tbody>{filas}</tbody>
+      <tbody>
+        {filas.map((fila) => (
+          <tr key={`fila-${fila}`}>
+            {columnas.map((columna) => {
+              const esCabeza = serpiente[0].x === columna && serpiente[0].y === fila;
+              const esCuerpo = serpiente.slice(1).filter(
+                (segmento) => segmento.x === columna && segmento.y === fila
+              ).length > 0;
+              const esComida = comida.x === columna && comida.y === fila;
+
+              let claseCelda = 'celda';
+              if (esCabeza) claseCelda += ' cabeza';
+              else if (esCuerpo) claseCelda += ' cuerpo';
+              else if (esComida) claseCelda += ' comida';
+
+              return (
+                <td key={`${fila}-${columna}`} className={claseCelda} />
+              );
+            })}
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
